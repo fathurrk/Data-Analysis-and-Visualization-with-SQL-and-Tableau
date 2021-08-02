9200f54ec628fe1e5821630f67b9b88958b4e15b
Fathurrachman Kadir <faturrahmankadir01@gmail.com>
2021-08-02 22:22:05 +0700

    Update sql_aggregate_function.sql

--- HR Sample Project/02. SQL Reporting with Aggregate Function/sql_aggregate_function.sql
+++ HR Sample Project/02. SQL Reporting with Aggregate Function/sql_aggregate_function.sql
@@ -6,8 +6,8 @@
   "Order by" for showing the highest salary.*/
   
 SELECT 		CONCAT(first_name, ' ', last_name) AS "Full Name", 
-          SUM(salary) AS salary
-FROM 		  employees
+         	SUM(salary) AS salary
+FROM 		employees
 GROUP BY	first_name, last_name
 ORDER BY	salary DESC;
 
@@ -16,21 +16,17 @@ ORDER BY	salary DESC;
   "Having" for select/filter only which value is 0 also you can use "WHERE" clause to select/filter "NULL" values.*/
   
 SELECT 		CONCAT(first_name, ' ', last_name) AS "Full Name", 
-			    COUNT(manager_id) AS manager_id
-FROM 		  employees
+		COUNT(manager_id) AS manager_id
+FROM 		employees
 --WHERE		manager_id IS NULL
 GROUP BY	first_name, last_name
 HAVING		COUNT(manager_id) = 0
 ORDER BY	manager_id DESC;
 
-/*Question 3 : Which employee didn't have "manager_id"?*/
-/*Note : "Count" for counting manager_id field,
-  "Having" for select/filter only which value is 0 also you can use "WHERE" clause to select/filter "NULL" values.*/
+/*Question 3 : Determine our average salary in 2020!*/
+/*Note : I use "AVG" to show average and also use "ROUND" to show without decimal.*/
   
-SELECT 		CONCAT(first_name, ' ', last_name) AS "Full Name", 
-			    COUNT(manager_id) AS manager_id
-FROM 		  employees
---WHERE		manager_id IS NULL
-GROUP BY	first_name, last_name
-HAVING		COUNT(manager_id) = 0
-ORDER BY	manager_id DESC;
+SELECT 		ROUND(AVG(salary)) AS salary
+FROM 		employees
+WHERE		EXTRACT(YEAR FROM hire_date) = 2000
+
