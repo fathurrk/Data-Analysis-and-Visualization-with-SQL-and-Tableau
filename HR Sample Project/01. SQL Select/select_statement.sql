abcfb350f7dcec42e9ab3164363b93c42f1e4a4f
Fathurrachman Kadir <faturrahmankadir01@gmail.com>
2021-08-02 20:19:44 +0700

    Update select_statement.sql

--- HR Sample Project/01. SQL Select/select_statement.sql
+++ HR Sample Project/01. SQL Select/select_statement.sql
@@ -10,21 +10,21 @@ WHERE		 first_name LIKE '%Alex%';
 /*Retrieve employee with last name is "Hunold" and "Khoo".*/
 SELECT 		*
 FROM 		 employees
-WHERE	  last_name IN ('Hunold', 'Khoo');
+WHERE	   last_name IN ('Hunold', 'Khoo');
 
 /*Retrieve employee with last name is "Hunold" and "Khoo".*/
 SELECT 		*
-FROM 		 employees
-WHERE		 last_name IN ('Hunold', 'Khoo');
+FROM 		  employees
+WHERE		  last_name IN ('Hunold', 'Khoo');
 
 /*Select all employee who is join in year "1995" and have salary higher than "10000".*/
 SELECT 		*
-FROM 		 employees
-WHERE		 EXTRACT(YEAR FROM hire_date) = 1995 AND salary > 10000 ;
+FROM 		  employees
+WHERE		  EXTRACT(YEAR FROM hire_date) = 1995 AND salary > 10000 ;
 
 /*Select all employee that earning salary between "5000" to "10000".*/
 SELECT 		*
-FROM 	  employees
+FROM 	   employees
 WHERE		 salary BETWEEN 5000 AND 10000 ;
 
 /*Sort all employee by highest salary.*/
