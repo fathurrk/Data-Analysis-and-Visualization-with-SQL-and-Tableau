4d80dd14ef8d5185c93cd580ee441f79d64d3caa
Fathurrachman Kadir <faturrahmankadir01@gmail.com>
2021-08-02 21:06:26 +0700

    Create sql_aggregate_function.sql

---
+++ HR Sample Project/02. SQL Aggregate Function/sql_aggregate_function.sql
@@ -0,0 +1,36 @@
+
+/*Question 1 : Which employee has the highest salary in company? */
+/*Note : i'm using "Concat" for merging the first_name and last_name so it will be "full name",
+  "Sum" for calculating the salary amount,
+  "Group by" for display field with aggregate function,
+  "Order by" for showing the highest salary.*/
+  
+SELECT 		CONCAT(first_name, ' ', last_name) AS "Full Name", 
+          SUM(salary) AS salary
+FROM 		  employees
+GROUP BY	first_name, last_name
+ORDER BY	salary DESC;
+
+/*Question 2 : Which employee didn't have "manager_id"?*/
+/*Note : "Count" for counting manager_id field,
+  "Having" for select/filter only which value is 0 also you can use "WHERE" clause to select/filter "NULL" values.*/
+  
+SELECT 		CONCAT(first_name, ' ', last_name) AS "Full Name", 
+			    COUNT(manager_id) AS manager_id
+FROM 		  employees
+--WHERE		manager_id IS NULL
+GROUP BY	first_name, last_name
+HAVING		COUNT(manager_id) = 0
+ORDER BY	manager_id DESC;
+
+/*Question 3 : Which employee didn't have "manager_id"?*/
+/*Note : "Count" for counting manager_id field,
+  "Having" for select/filter only which value is 0 also you can use "WHERE" clause to select/filter "NULL" values.*/
+  
+SELECT 		CONCAT(first_name, ' ', last_name) AS "Full Name", 
+			    COUNT(manager_id) AS manager_id
+FROM 		  employees
+--WHERE		manager_id IS NULL
+GROUP BY	first_name, last_name
+HAVING		COUNT(manager_id) = 0
+ORDER BY	manager_id DESC;
